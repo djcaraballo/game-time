dbcf5cc16e174e3d4a29762ba72a39f2081b159f
“Freddie <freddielev@gmail.com>
2018-08-25 17:39:22 -0600

    add self collision test

--- test/Game-test.js
+++ test/Game-test.js
@@ -17,17 +17,32 @@ describe('Game', () => {
   it('should end the game if block collides with wall', () => {
 
     const game = new Game(ctx);
-    game.gameOver = false;
     const block1 = new Block (300, 30, 10, 10);
-    // block.isCollidingWithWall(300, 300);
-    // game.endGame();
 
+    assert.equal(game.gameOver, false)
     assert.equal(block1.isCollidingWithWall(300, 300), true);
+
     game.handleBlock(block1);
+
     assert.equal(game.gameOver, true)
 
   })
   
+  it('should end game if player collides with own trail', () => {
+    
+    const game = new Game(ctx);
+    const block1 = new Block (39, 30, 10, 10);
+
+    assert.equal(game.gameOver, false);
+
+    block1.path = [ {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}, {x: 38, y: 30, width: 10, height:10}];
+    block1.isCollidingWithOwnPath(block1.path);
+
+    assert.equal(block1.isCollidingWithOwnPath(block1.path), true)
+
+
+
+  })
   // it.skip('should take properties', () => {})
   // it.skip('should collide with walls', () => {})
   // it.skip('should be able to move', () => {})
@@ -45,7 +60,6 @@ describe('Game', () => {
     assert.equal(this.gameOn, true)
   })
 
-  it.skip('should end game if player collides with own trail')
   it.skip('should end game if player collides with other trail')
   it.skip('should set game Over to true if end game is called')
   it.skip('should toggle paused state if togglePause is called')
