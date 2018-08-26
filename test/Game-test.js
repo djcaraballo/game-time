d1f0c7fe93b867534907baebf65e31fd0cbeb996
Dina <dcaraballo26@gmail.com>
2018-08-26 01:45:22 -0600

    Refactor code in game-test file for readbility

--- test/Game-test.js
+++ test/Game-test.js
@@ -17,20 +17,16 @@ describe('Game', () => {
   it('should end the game if block collides with wall', () => {
     const game = new Game(ctx);
     const block1 = new Block (300, 30, 10, 10);
-
-    assert.equal(game.gameOver, false)
+    assert.equal(game.gameOver, false);
     assert.equal(block1.isCollidingWithWall(300, 300), true);
 
     game.handleBlock(block1);
-
     assert.equal(game.gameOver, true)
-
   })
   
   it('should end game if player collides with own trail', () => {
     const game = new Game(ctx);
     const block1 = new Block (39, 30, 10, 10);
-
     assert.equal(game.gameOver, false);
 
     block1.path = [ 
@@ -58,23 +54,21 @@ describe('Game', () => {
       {x: 38, y: 30, width: 10, height:10}
       ];
 
-
     assert.equal(block1.isCollidingWithOwnPath(block1.path), true);
 
     game.handleBlock(block1);
-
     assert.equal(game.gameOver,  true)
   })
   
   it('should have a default status of off', () => {
     const game = new Game();
-
     assert.equal(game.gameOver, false) 
   })
 
   it('should toggle game status', () => {
     const game = new Game(ctx);
     assert.equal(game.gameOver, false)
+    
     game.endGame();
     assert.equal(game.gameOver, true)
   }) 
@@ -83,12 +77,9 @@ describe('Game', () => {
     const game = new Game(ctx);
     const block1 = new Block (38, 30, 10, 10);
     const block2 = new Block (25, 55, 10, 10);
-
-
     assert.equal(game.gameOver, false);
 
     block1.path = []
-
     block2.path = [ 
       {x: 38, y: 30, width: 10, height:10}, 
       {x: 38, y: 30, width: 10, height:10}, 
@@ -119,35 +110,28 @@ describe('Game', () => {
     game.blocks = [block1, block2];
 
     game.handleBlock(block1);
-
-    assert.equal(game.gameOver, true)
-
+    assert.equal(game.gameOver, true);
   })
 
   it('should set game over to true if end game is called', () => {
-    const game = new Game(ctx)
-
-    assert.equal(game.gameOver, false)
+    const game = new Game(ctx);
+    assert.equal(game.gameOver, false);
 
     game.endGame();
-
-    assert.equal(game.gameOver, true)
+    assert.equal(game.gameOver, true);
   })
 
   it('should toggle paused state if togglePause is called', () => {
     const game = new Game(ctx)
-
     assert.equal(game.paused, false);
 
     game.togglePause();
-
     assert.equal(game.paused, true);
   })
 
   it('should change Direction of path on key press', () => {
     const game = new Game(ctx)
-    
-  })
   // create an event object (let event = {key: 'ArrowRight'}
-  // assert.equal(game.blocks[1].x = the coordinate it should have when arrow right is pressed)
+  // assert.equal(game.blocks[1].x = the coordinate it should have when arrow right is pressed)    
+  })
 })
